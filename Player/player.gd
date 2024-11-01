919cf8a24be507c8e65d0dcbde00250b88307964
caioaex <caioexteckoetter0@gmail.com>
2024-11-01 19:21:32 -0300

    Adicionando player, inimigos, etc...

---
+++ Player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+class_name Player
+
+@export_category("Variables")
+@export var _move_speed: float = 256.0
+
+func _physics_process(_delta):
+	_move()
+
+func _move() -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * _move_speed
+	move_and_slide()
